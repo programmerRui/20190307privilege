0c3f922e848ede4de7a4af2923e27440c5ad3a28
programmerRui <48912282+programmerRui@users.noreply.github.com>
2019-03-27 10:44:56 +0800

    Update text.java

--- src/com/privilege/text/text.java
+++ src/com/privilege/text/text.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class text {
 
     public static void main(String[] args) {
+        
         QueryRunner queryRunner= C3P0Util.getQueryRunner();
         String sql="select *from \"privileges\" ";
         List<Privileges> query=null;
